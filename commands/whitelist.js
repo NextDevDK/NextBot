21dc4a5f0706b8e0ed72f654eb80088cf489bf97
Theis Jensen <mbrock399@gmail.com>
2020-10-09 16:09:57 +0200

    Added whitelist

---
+++ commands/whitelist.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'whitelist',
+    description: 'Whitelist user',
+    async execute(message, args, con) {
+        if (!message.channel.name == "admin") return message.channel.send("Please write this in the admin channel!!!");
+
+        var psAdd = 'UPDATE user_data SET WHITELISTED=1 WHERE NAME=?';
+        var psRemove = 'UPDATE user_data SET WHITELISTED=0 WHERE NAME=?';
+        switch(args[0]) {
+            case "add":
+                await con.query(ps, args[1], function (err, result) {
+                    if (err) throw err;
+                    message.channel.send("Added "+args[1]+" To Whitelist!");
+                });
+            break;
+            case "remove":
+                await con.query(ps, args[1], function (err, result) {
+                    if (err) throw err;
+                    message.channel.send("Removed "+args[1]+" From Whitelist!");
+                });
+            break;
+        }
+    },
+};
