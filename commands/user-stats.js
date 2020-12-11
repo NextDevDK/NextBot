4e622be273913189712c636739955302c5645d1c
Theis N. Jensen <mbrock399@gmail.com>
2020-12-12 00:04:31 +0100

    Added old API Back

--- commands/user-stats.js
+++ commands/user-stats.js
@@ -8,15 +8,13 @@ module.exports = {
             if (message.channel.name == "admin") {
                 await message.channel.send("Søger efter " + args[0] + " i databasen.");
 
-                await axios.get('https://api.oz1tnj.dk/skynilla/player/' + args[0])
+                await axios.get('http://api.oz1tnj.dk/skynilla/player/' + args[0])
                     .then(function (res) {
-                        data = res.data.data[0];
-
-                        var dateResult = new Date(data.playtime * 1000).toISOString().substr(11, 8);
-                        var avatarUrl = "https://minotar.net/helm/" + data.uuid + "/128";
+                        var dateResult = new Date(res.data.PLAYTIME * 1000).toISOString().substr(11, 8);
+                        var avatarUrl = "https://minotar.net/helm/" + res.data.UUID + "/128";
                         var whitelisted
 
-                        switch (data.whitelisted) {
+                        switch (res.data.WHITELISTED) {
                             case 1:
                                 whitelisted = "Yes"
                                 break;
@@ -31,67 +29,66 @@ module.exports = {
                         const embed = new Discord.MessageEmbed()
                             .setColor('#FF0000')
                             .setTitle('SkyNilla Admin Spiller info')
-                            .setAuthor(data.name, avatarUrl)
+                            .setAuthor(res.data.NAME, avatarUrl)
                             .addFields({
                                 name: 'Spiller Navn',
-                                value: data.name
+                                value: res.data.NAME
                             }, {
                                 name: 'Spiller UUID',
-                                value: data.uuid
+                                value: res.data.UUID
                             }, {
                                 name: 'Spiller IP',
-                                value: data.ip
+                                value: res.data.IP
                             }, {
                                 name: 'Spiller Whitelisted',
                                 value: whitelisted
                             }, {
                                 name: 'Sidst Logget På',
-                                value: data.lastseen
+                                value: res.data.LASTSEEN
                             }, {
                                 name: 'Total Spilletid',
                                 value: dateResult
                             }, {
                                 name: 'Antal Gange Død',
-                                value: data.death
+                                value: res.data.DEATH
                             })
                             .setTimestamp()
                             .setFooter('Lavet Af mrcool1575 Og Toby');
 
                         message.channel.send(embed);
                     })
-                    .catch(function (error) { message.reply("Vi kunne ikke finde " + args[0] + " i vores database. Prøv igen!" + error); });
+                    .catch(function (error) { message.reply("Vi kunne ikke finde " + args[0] + " i vores database. Prøv igen!"); });
             }
         } else {
             await message.channel.send("Søger efter " + args[0] + " i databasen.");
-            await axios.get('https://api.oz1tnj.dk/skynilla/player/' + args[0])
+            await axios.get('http://api.oz1tnj.dk/skynilla/player/' + args[0])
                 .then(function (res) {
-                    data = res.data.data[0];
-                    var dateResult = new Date(data.playtime * 1000).toISOString().substr(11, 8);
-                    var avatarUrl = "https://minotar.net/helm/" + data.uuid + "/128";
+                    var dateResult = new Date(res.data.PLAYTIME * 1000).toISOString().substr(11, 8);
+                    var avatarUrl = "https://minotar.net/helm/" + res.data.UUID + "/128";
 
                     const embed = new Discord.MessageEmbed()
                         .setColor('#0099ff')
                         .setTitle('SkyNilla Spiller info')
-                        .setAuthor(data.name, avatarUrl)
+                        .setAuthor(res.data.NAME, avatarUrl)
                         .addFields({
                             name: 'Spiller Navn',
-                            value: data.name
+                            value: res.data.NAME
                         }, {
                             name: 'Sidst Logget På',
-                            value: data.lastseen
+                            value: res.data.LASTSEEN
                         }, {
                             name: 'Total Spilletid',
                             value: dateResult
                         }, {
                             name: 'Antal Gange Død',
-                            value: data.death
+                            value: res.data.DEATH
                         })
                         .setTimestamp()
                         .setFooter('Lavet Af mrcool1575 Og Toby');
 
                     message.channel.send(embed);
                 })
-                .catch(function (error) { message.reply("Vi kunne ikke finde " + args[0] + " i vores database. Prøv igen!" + error); });
+                .catch(function (error) { message.reply("Vi kunne ikke finde " + args[0] + " i vores database. Prøv igen!"); });
         }
     },
 };
